ed6cca00bb5d0b26839c4433f7f702839f648c91
Yuichi Tanaka <yuichielectric@github.com>
2021-07-12 08:17:07 +0000

    Eval user input

--- index.js
+++ index.js
@@ -68,7 +68,7 @@
    var from = req.params.from;
    var to = req.params.to;
    var names = users.map(function(user){ return user.name; });
-   eval(names);
+   eval(from);
    res.send('users ' + names.slice(from, to + 1).join(', '));
  });
  
