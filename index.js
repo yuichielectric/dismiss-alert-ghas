81851bba0c3e6daea96cfbd3bafcbe34a535b71c
Yuichi Tanaka <yuichielectric@github.com>
2021-07-12 09:17:22 +0000

    Add vulnerable code

--- index.js
+++ index.js
@@ -78,3 +78,13 @@
    app.listen(3000);
    console.log('Express started on port 3000');
  }
+
+ app.get('/some/path', function(req, res) {
+  let url = req.param('url'),
+      host = urlLib.parse(url).host;
+  // BAD: the host of `url` may be controlled by an attacker
+  let regex = /^((www|beta).)?example.com/;
+  if (host.match(regex)) {
+      res.redirect(url);
+  }
+});
